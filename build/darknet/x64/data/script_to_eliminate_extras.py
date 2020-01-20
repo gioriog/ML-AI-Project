a42935a4f62bf5d23180dda6525cee8cc4491913
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2020-01-20 22:27:56 +0100

    Caricamento Comic

---
+++ build/darknet/x64/data/script_to_eliminate_extras.py
@@ -0,0 +1,39 @@
+import os
+from os import path
+import shutil
+
+def copyFiles(src,dst,file,ext):
+	with open(file) as f:
+		files = f.readlines()
+
+	# eliminate \n
+	files = [f.strip() for f in files]
+
+	# add jpg extension
+	img_files = [f+ext for f in files]
+        
+	print(files)
+	
+	#sys.stdout.write("".join(files))
+
+	# copy from src to dst
+	for f in files:
+		shutil.copy(path.join(src,f), dst)
+
+	
+
+train_path = "comic/ImageSets/Main/train.txt"
+test_path = "comic/ImageSets/Main/test.txt"
+
+src = "comic/JPEGImages/"
+dst = "comic/JPEGImages2/"
+
+copyFiles(src,dst,train_path,".jpg")
+copyFiles(src,dst,test_path,".jpg")
+
+src = "comic/Annotations/"
+dst = "comic/Annotations2/"
+
+copyFiles(src,dst,train_path,".xml")
+copyFiles(src,dst,test_path,".xml")
+
