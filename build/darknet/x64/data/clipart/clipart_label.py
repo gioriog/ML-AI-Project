3f50befc06fdde039a3a8a853b012555be9fbf8e
gioriog <45177062+gioriog@users.noreply.github.com>
2020-01-21 23:24:59 +0100

    Update

--- build/darknet/x64/data/clipart/clipart_label.py
+++ build/darknet/x64/data/clipart/clipart_label.py
@@ -49,9 +49,12 @@ for image_set in sets:
         os.makedirs('labels/')
     #image_ids = open('ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     #list_file = open('%s_%s.txt'%(year, image_set), 'w')
-    image_ids = open('ImageSets/Main/%s.txt'%(image_set)).read().strip()
+    image_ids = open('ImageSets/Main/%s.txt'%(image_set)).readlines()
+  
     list_file = open('%s.txt'%(image_set), 'w')
     for image_id in image_ids:
-        list_file.write('JPEGImages/%s.jpg\n'%(image_id))
+        image_id = image_id.strip()
+
+        list_file.write(wd +'/JPEGImages/%s.jpg\n'%(image_id))
         convert_annotation(image_id)
     list_file.close()
