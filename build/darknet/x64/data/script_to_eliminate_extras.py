1deae5d517c015099297900f3a08557e1a85f814
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2020-02-08 10:24:50 +0100

    Rimozione extra

--- build/darknet/x64/data/script_to_eliminate_extras.py
+++ build/darknet/x64/data/script_to_eliminate_extras.py
@@ -17,11 +17,11 @@ def copyFiles(src,dst,file,ext):
 	#sys.stdout.write("".join(files))
 
 	# copy from src to dst
-	for f in files:
+	for f in img_files:
 		shutil.copy(path.join(src,f), dst)
 
 	
-
+'''
 train_path = "comic/ImageSets/Main/train.txt"
 test_path = "comic/ImageSets/Main/test.txt"
 
@@ -33,6 +33,27 @@ copyFiles(src,dst,test_path,".jpg")
 
 src = "comic/Annotations/"
 dst = "comic/Annotations2/"
+
+copyFiles(src,dst,train_path,".xml")
+copyFiles(src,dst,test_path,".xml")
+
+'''
+
+
+
+#WATERCOLOR
+
+train_path = "watercolor/ImageSets/Main/train.txt"
+test_path = "watercolor/ImageSets/Main/test.txt"
+
+src = "watercolor/JPEGImages/"
+dst = "watercolor/JPEGImages2/"
+
+copyFiles(src,dst,train_path,".jpg")
+copyFiles(src,dst,test_path,".jpg")
+
+src = "watercolor/Annotations/"
+dst = "watercolor/Annotations2/"
 
 copyFiles(src,dst,train_path,".xml")
 copyFiles(src,dst,test_path,".xml")
